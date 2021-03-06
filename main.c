118303cfc106d591820a9d9d36cab4b1b73e5d9c
Will Bettridge-Radford <william.bettridge.radford@gmail.com>
2021-03-06 20:13:28 +0000

    wip SDL shim

--- main.c
+++ main.c
@@ -1,196 +1,67 @@
+#ifdef SDL
+
+#else
+
 #include <gb/gb.h>
+#include <gb/bgb_emu.h>
 #include <gb/cgb.h>
+#include <string.h>
+#include <stdio.h>
 
-#define MAP_WIDTH 32;
-#define MAP_HEIGHT 32;
+#endif
 
-unsigned char t = 0;
-unsigned char i = 0;
-unsigned char x = 0;
-unsigned char y = 0;
+#include "shared.h"
 
-unsigned char *screen = NULL;
-unsigned char *iscreen[256];
-unsigned char tile[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-unsigned char j = 0;
+u8 t = 0;
+i8 i = 0;
+u8 x = 0;
+u8 y = 0;
 
-enum direction {
-    right,
-    down,
-    left,
-    up,
-};
+struct gb_tile tile;
+u8 *screen = NULL;
+u8 *iscreen[256];
+u8 j = 0;
+char msg_buf[128];
 
-unsigned char
-pget(char *t, char x, char y)
-{
-    unsigned char mask;
-    unsigned char *row;
-    mask = 0b10000000 >> x;
-    row = t + (y * 2);
-    return (*row & mask ? 1 : 0) + (*(row+1) & mask ? 2 : 0);
-}
-
-void
-pset(char *t, char x, char y, char p)
-{
-    unsigned char mask;
-    unsigned char *row;
-    mask = 0b10000000 >> x;
-    row = t + (y * 2);
-    *row |= (mask & p);
-    row += 1;
-    *row |= (mask & p);
-}
 
 void
 init(void)
 {
-    signed char k = 0;
-    char x = 3;
-    char y = 3;
-    unsigned char mask = 0;
-    unsigned char *row = NULL;
-    unsigned char p = 0;
-    enum direction d = right;
-    unsigned char i = 0;
     unsigned int j = 0;
+    u8 i = 0;
+    u8 x = 0;
+    u8 y = 0;
+    u8 c = 0;
 
+    memset(&tile, 0, sizeof tile);
 
-    k = -127;
-    do {
-        row = &tile[y * 2];
-
-        mask = 0b10000000 >> x;
-
-        p  = *row & mask ? 1 : 0;
-        p += *(row+1) & mask ? 2 : 0;
-
-        switch (p) {
-            case 0:
-                *row |= mask;
-                break;
-            case 1:
-                /*turn off*/
-                *row &= ~mask;
-                *(row+1) |= mask;
-                /*turn on*/
-                break;
-            case 2:
-                *row |= mask;
-            case 3:
-                /*move cursor*/
-                switch (i) {
-                    case 0:
-                        x += 1;
-                        break;
-                    case 1:
-                        y += 1;
-                        break;
-                    case 2:
-                    case 3:
-                        x -= 1;
-                        break;
-                    case 4:
-                    case 5:
-                        y -= 1;
-                        break;
-                    case 6:
-                    case 7:
-                    case 8:
-                        x += 1;
-                        break;
-                    case 9:
-                    case 10:
-                    case 11:
-                        y += 1;
-                        break;
-                    case 12:
-                    case 13:
-                    case 14:
-                    case 15:
-                        x -= 1;
-                        break;
-                    case 16:
-                    case 17:
-                    case 18:
-                    case 19:
-                        y -= 1;
-                        break;
-                    case 20:
-                    case 21:
-                    case 22:
-                    case 23:
-                    case 24:
-                        x += 1;
-                        break;
-                    case 25:
-                    case 26:
-                    case 27:
-                    case 28:
-                    case 29:
-                        y += 1;
-                        break;
-                    case 30:
-                    case 31:
-                    case 32:
-                    case 33:
-                    case 34:
-                    case 35:
-                        x -= 1;
-                        break;
-                    case 36:
-                    case 37:
-                    case 38:
-                    case 39:
-                    case 40:
-                    case 41:
-                        y -= 1;
-                        break;
-                    case 42:
-                    case 43:
-                    case 44:
-                    case 45:
-                    case 46:
-                    case 47:
-                    case 48:
-                        x += 1;
-                        break;
-                    case 49:
-                    case 50:
-                    case 51:
-                    case 52:
-                    case 53:
-                    case 54:
-                    case 55:
-                        y += 1;
-                        break;
-                    case 56:
-                    case 57:
-                    case 58:
-                    case 59:
-                    case 60:
-                    case 61:
-                    case 62:
-                        x -= 1;
-                        break;
-                    default:
-                        /*y += 1;*/
-                        break;
-                }
-                i += 1;
-                break;
-
+    for (i = 0; i != 128; i += 1) {
+        if (x == 15) {
+            x = 0;
+            y += 1;
         }
 
+        if (y == 15) {
+            y = 0;
+        }
 
-        set_bkg_data(k, 1, (void*)&tile);
-        k += 1;
-    } while (k != -128);
+        /*pset(&tile, x, y, ++c & 3);*/
+        /*x += 1;*/
+
+        pset(&tile, ++x & 15, ++y % 15, ++c & 3);
+        for (y = 0; y != 16; y += 1) {
+            for (x = 0; x != 16; x += 1) {
+                c = closest_colour(x, y, i);
+                pset(&tile, x, y, c);
+            }
+        }
+
+        set_bkg_data(i + 0, 1, (void*)&tile);
+    }
 
     screen = (unsigned char *)0x9800;
     for (j = 1024; j; j -= 1)  {
-        *screen = 0x4f;
+        *screen = 0x7f;
         screen += 1;
     }
 
@@ -213,6 +84,7 @@ main(void)
 
     screen = (unsigned char *)0x9800;
     screen += MAP_WIDTH;
+
     for (y = 0; y != 16; y += 1) {
         screen += 2;
         for (x = 0; x != 16; x += 1) {
@@ -229,13 +101,15 @@ main(void)
 
     for (;;) {
 
-#define tiles_per_frame 41
+#define tiles_per_frame 16
 
         j = tiles_per_frame;
 
+        /*BGB_MESSAGE_FMT(msg_buf, "i: %d", i);*/
+
         wait_vbl_done();
         while (j--) {
-            if (p >= p_end)
+            if (p == p_end)
                 p = p_start;
 
             if (x == 16) {
@@ -244,12 +118,12 @@ main(void)
             }
 
             y &= 0x0f;
-            /*if (y == 16)*/
-                /*y = 0;*/
 
-            **(p++) = i + t + x >> 2;
+#define CMD (i + t) + x >> 3
+            **(p++) = (CMD) & 0x7f;
 
             x += 1;
+            i += 1;
         }
         t += 1;
     }
---
+++ shared.h
@@ -0,0 +1,105 @@
+#define MAP_WIDTH 32;
+#define MAP_HEIGHT 32;
+
+typedef unsigned char u8;
+typedef   signed char i8;
+
+struct gb_row {
+    u8 low;
+    u8 high;
+};
+
+
+struct gb_tile {
+    struct gb_row row[8];
+};
+
+u8 dither_42[8][8] = {
+    {  0,  20,   5,  26,   1,  22,   6,  27},
+    { 31,  10,  36,  15,  32,  11,  37,  16},
+    {  7,  28,   2,  23,   9,  29,   3,  24},
+    { 39,  18,  33,  13,  40,  19,  35,  14},
+    {  1,  22,   7,  27,   0,  21,   5,  26},
+    { 33,  12,  38,  17,  31,  11,  37,  16},
+    {  9,  30,   4,  25,   8,  29,   3,  24},
+    { 41,  20,  35,  14,  39,  18,  34,  13}
+};
+
+
+#define number_of_colours 4
+#define number_of_sectors (number_of_colours - 1)
+#define max_val 127
+#define sector_size (max_val / number_of_sectors)
+
+
+u8
+closest_colour(u8 x, u8 y, i8 c)
+{
+    u8 s = 0;
+    i8 limit = dither_42[y % 8][x % 8];
+    i8 sect = c / sector_size;
+
+    if (c < 0)
+        c = -c;
+
+    s = (c % sector_size) > limit ? sect + 1 : sect;
+    /*s = c > limit ? sect + 1 : sect;*/
+    s = s > 3 ? 3 : s;
+
+    return s;
+}
+
+
+u8
+pget(struct gb_tile *t, u8 x, u8 y)
+{
+    struct gb_row *r = &t->row[y];
+    u8 mask = 0;
+    mask = 1 << x;
+    return
+        (r->low  && mask ? 1 : 0)
+        +
+        (r->high && mask ? 2 : 0);
+}
+
+
+void
+pset(struct gb_tile *t, u8 x, u8 y, u8 c)
+{
+    struct gb_row *r = &t->row[y];
+    u8 mask = 0x80 >> x;
+    /*u8 mask = 1 << x;*/
+    /*u8 mask = 0b10000000 >> x;*/
+    u8 *l = &r->low;
+    u8 *h = &r->high;
+
+    *l = (c & 1) ? (*l | mask) : (*l & ~mask);
+    *h = (c & 2) ? (*h | mask) : (*h & ~mask);
+
+
+}
+
+
+/*unsigned char*/
+/*pget(char *t, char x, char y)*/
+/*{*/
+    /*unsigned char mask;*/
+    /*unsigned char *row;*/
+    /*mask = 0b10000000 >> x;*/
+    /*row = t + (y * 2);*/
+    /*return (*row & mask ? 1 : 0) + (*(row+1) & mask ? 2 : 0);*/
+/*}*/
+
+/*void*/
+/*pset(char *t, char x, char y, char p)*/
+/*{*/
+    /*unsigned char mask;*/
+    /*unsigned char *row;*/
+    /*mask = 0b10000000 >> x;*/
+    /*row = t + (y * 2);*/
+    /**row |= (mask & p);*/
+    /*row += 1;*/
+    /**row |= (mask & p);*/
+/*}*/
+
+
